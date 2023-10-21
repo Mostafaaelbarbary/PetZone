92bf35416892af63dc263e687efc7bc6a2d041f2
Mohamed Hesham <101864682+MohamedBoghdaddy@users.noreply.github.com>
2023-10-21 20:07:25 +0300

    Add files via upload

---
+++ patients.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Patients</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href ="style/patients.css">
+
+
+</head>
+<body>
+
+
+       
+        <div class = "styles-patients = display: flex;">
+        <p>Patients</p> 
+        <form method="POST" action="create-patient.php">  
+        <input class="add-patient = display: flex;" type="submit" name = "+ Add Patient" value="+ Add Patient" required> 
+        </form>
+        </div>
+      
+        
+
+</body>
+</html>
