0403f75e270e0bdfe8ea39f07385be5f1ae37d18
Mohamed Matar <148243612+mohamedSalehMatar@users.noreply.github.com>
2023-10-24 20:04:12 +0300

    Update dashboard.php
    to be able to load the css dashboard file

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <title>Vet Clinic Dashboard</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="styledash.css" />
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
