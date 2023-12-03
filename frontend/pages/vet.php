fe6a620fcb59b3b566dac20dc58a8affa001c941
Adham Said <adham.soliman725@gmail.com>
2023-12-04 00:34:33 +0200

    123

---
+++ frontend/pages/vet.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Shop</title>
+    <link rel="stylesheet" href="style/vet.css">
+</head>
+<body>
+    <h1>Welcome to our Online Shop</h1>
+   <div class="container1">
+   <div class="content1">
+        <div class="product">
+           <h2>Product 1</h2> 
+           <img src="toys/dog-toys1.jpg"
+            alt="Product 1">        
+            <p>$10</p>
+            <input type="number" name="product1_quantity" value="0" min="0">
+        </div>
+</div>
+        <div class="product">
+            <h2>Product 2</h2> 
+            <img src="toys/dog-toys2.png"
+             alt="Product 2">         
+            <p>$15</p>
+            <input type="number" name="product2_quantity" value="0" min="0">
+        </div>
+   
+   </div>
+   <div class="container2">
+   <div class="content2">
+        <div class="product">
+             <h2>Product 3</h2>
+            <img src="pet food/202713_MAIN._AC_SS100_V1669909139_.jpg"
+             alt="Product 3">          
+            <p>$15</p>
+            <input type="number" name="product3_quantity" value="0" min="0">
+        </div>
+</div>
+        <div class="product">
+            <h2>Product 4</h2> 
+             <img src="pet food/foodcat.jpg" 
+             alt="Product 4">          
+            <p>$15</p>
+            <input type="number" name="product4_quantity" value="0" min="0">
+        </div>
+   
+   </div>
+
+        <input type="submit" value="Add to Cart">
+    
+    <h1>Your order has been placed successfully!</h1>
+    <p>Thank you for shopping with us.</p>
+</body>
+
+ 
+   
+
+</html>
